a3d093a978a31738984e4be34505bd6364d19370
Logan Crewss <logancrewss@Blakes-MacBook-Air.local>
2017-07-25 14:48:31 -0600

    add cors

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var route = require('./routes/liquor_routes')
 var port = process.env.PORT || 3000
+var cors = require('cors')
+
+
+app.use(cors())
 
 app.use('/', route)
 
